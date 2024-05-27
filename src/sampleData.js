6c81b2b80eac587851498afd72e94fd942b7ccae
Abinash Yadav <avinashy38@gmail.com>
2024-05-27 18:40:27 +0530

    first commit

---
+++ src/sampleData.js
@@ -0,0 +1,39 @@
+export const posts = [
+  {
+    heading: "Dummy post sample heading 1",
+    id: "1",
+    creator: "Adam",
+    avatar: "https://github.com/shadcn.png",
+    body: "",
+    createdAt: "2024-05-26T20:32:52.973Z",
+    category:"Technology",
+    cardThumbnail:"test.jpeg",
+    isDummy: true,
+  },
+  {
+    heading: "Dummy post sample heading 2",
+    id: "2",
+    creator: "Leon",
+    avatar: "https://avatars.githubusercontent.com/u/41151614?v=4",
+    body: "",
+    createdAt: "2024-05-26T20:32:52.973Z",
+    category:"Technology",
+    cardThumbnail:"test-1.jpg",
+    isDummy: true,
+  },
+  {
+    heading: "Dummy post sample heading 3",
+    id: "3",
+    creator: "Max",
+    avatar: "https://avatars.githubusercontent.com/u/88678143?v=4",
+    body: "",
+    createdAt: "2024-05-26T20:32:52.973Z",
+    category:"Technology",
+    cardThumbnail:"test-2.jpg",
+    isDummy: true,
+  },
+]
+
+
+
+
